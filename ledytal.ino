8e60679f9bffb8572530e92808918d5027a84ab4
Emanuel Gutiérrez <142856302+VanillaCow14@users.noreply.github.com>
2024-05-17 15:59:18 -0600

    Add files via upload

---
+++ ledytal.ino
@@ -0,0 +1,109 @@
+#include <WiFi.h>
+
+//------------------Servidor Web en puerto 80---------------------
+
+WiFiServer server(80);
+
+//---------------------Credenciales de WiFi-----------------------
+
+const char* ssid     = "INFINITUM559C";
+const char* password = "Enterasys2024";
+
+//---------------------VARIABLES GLOBALES-------------------------
+int contconexion = 0;
+
+String header; // Variable para guardar el HTTP request
+
+String estadoSalida = "off";
+
+const int salida = 14;
+
+//------------------------CODIGO HTML------------------------------
+String pagina = "<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<meta charset='utf-8' />"
+"<title>Servidor Web ESP32</title>"
+"</head>"
+"<body>"
+"<center>"
+"<h1>Servidor Web ESP32</h1>"
+"<p><a href='/on'><button style='height:50px;width:100px'>ON</button></a></p>"
+"<p><a href='/off'><button style='height:50px;width:100px'>OFF</button></a></p>"
+"</center>"
+"</body>"
+"</html>";
+
+
+//---------------------------SETUP--------------------------------
+void setup() {
+  Serial.begin(115200);
+  Serial.println("");
+  
+  pinMode(salida, OUTPUT); 
+  digitalWrite(salida, LOW);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED and contconexion <50) { 
+    ++contconexion;
+    delay(500);
+    Serial.print(".");
+  }
+  if (contconexion <50) {
+      Serial.println("");
+      Serial.println("WiFi conectado");
+      Serial.println(WiFi.localIP());
+      server.begin(); 
+  }
+  else { 
+      Serial.println("");
+      Serial.println("Error de conexion");
+  }
+}
+
+//----------------------------LOOP----------------------------------
+
+void loop(){
+  WiFiClient client = server.available();   // Escucha a los clientes entrantes
+
+  if (client) {                             // Si se conecta un nuevo cliente
+    Serial.println("New Client.");          // 
+    String currentLine = "";                //
+    while (client.connected()) {            // loop mientras el cliente está conectado
+      if (client.available()) {             // si hay bytes para leer desde el cliente
+        char c = client.read();             // lee un byte
+        Serial.write(c);                    // imprime ese byte en el monitor serial
+        header += c;
+        if (c == '\n') {                    
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+     
+            if (header.indexOf("GET /on") >= 0) {
+              Serial.println("GPIO on");
+              estadoSalida = "on";
+              digitalWrite(salida, HIGH);
+            } else if (header.indexOf("GET /off") >= 0) {
+              Serial.println("GPIO off");
+              estadoSalida = "off";
+              digitalWrite(salida, LOW);
+            }
+            client.println(pagina);
+            client.println();
+            break;
+          } else { 
+            currentLine = "";
+          }
+        } else if (c != '\r') {  
+          currentLine += c;      
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
